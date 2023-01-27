79d68a234bae4d373390b404d70d3e7547c71188
Ravinder <93916538+Ravinder-Khera@users.noreply.github.com>
2023-01-27 16:36:31 +0530

    banner image added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89665340b7fdf9aa"; }
+/******/ 		__webpack_require__.h = function() { return "9421e50cd2b06c12"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
