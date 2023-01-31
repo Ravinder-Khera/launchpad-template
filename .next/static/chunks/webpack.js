e70489b43152364c682a6d3cd1eebf9b4e58ed6d
Ravinder <93916538+Ravinder-Khera@users.noreply.github.com>
2023-01-31 12:32:12 +0530

    footer social

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "564ab428b5a093ab"; }
+/******/ 		__webpack_require__.h = function() { return "48677ec86eaccb47"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
