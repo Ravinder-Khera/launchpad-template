17ed98acf4e81b7405f2cdeb1118a9f7354dcc19
Ravinder <93916538+Ravinder-Khera@users.noreply.github.com>
2023-01-31 11:11:35 +0530

    slider classes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a68fc74ce136afca"; }
+/******/ 		__webpack_require__.h = function() { return "62d24ac89fa1789a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
